5764975c28a622820c58c9a1c4de32514ae2be07
Liam Sutton <devliam90@gmail.com>
2020-08-06 15:26:56 +0100

    addrockets data

---
+++ basic-server/src/data/rockets.js
@@ -0,0 +1,34 @@
+const rockets = [
+  {
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Falcon_Heavy_Demo_Mission_%2839337245145%29.jpg/220px-Falcon_Heavy_Demo_Mission_%2839337245145%29.jpg',
+    country: 'USA',
+    description:
+      'Falcon Heavy is a resuable super heavy-lift launch vehicle designed and manufactured by SpaceX',
+    name: 'Falcon Heavy',
+  },
+  {
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/ENERGIJA.png/220px-ENERGIJA.png',
+    country: 'Russia',
+    description:
+      'Energia was a Soviet rocket that was designed by NPO Energia to serve as a heavy-lift partially recoverable launch system for a variety of payloads including the Buran spacecraft',
+    name: 'Energia',
+  },
+  {
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Sls_block1_on-pad_sunrisesmall.jpg/220px-Sls_block1_on-pad_sunrisesmall.jpg',
+    country: 'USA',
+    description:
+      'The Space Launch System (SLS) is a super heavy-lift expendable launch vehicle, which has been under development by NASA in the United States since its announcement in 2011',
+    name: 'SLS Launch System',
+  },
+  {
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/BFR_in_orbit%2C_2017.PNG/220px-BFR_in_orbit%2C_2017.PNG',
+    country: 'USA',
+    description:
+      'In September 2017, at the 68th annual meeting of the International Astronautical Congress, SpaceX unveiled the updated vehicle design. Musk said, "we are searching for the right name, but the code name, at least, is BFR."',
+    name: 'BFR (Big Falcon Rocket)',
+  },
+];
